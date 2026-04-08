b79871aabcf818d2f3515ba949d9667c531cd997
hyma999 <hymaaritakula93@gmail.com>
2026-04-08 14:55:37 +0530

    added

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "terraform_data" "main" {
     type     = "ssh"
     user     = "ec2-user"
     password = "DevOps321"
-    host     = aws_instance.main.private_ip
+    host     = aws_instance.main.public_ip
   }
 
   provisioner "file" {
